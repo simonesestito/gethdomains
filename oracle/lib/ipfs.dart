04096d43d088cc9a811fa603c5ffd14b4f592455
Simone Sestito <simone@simonesestito.com>
2024-01-15 11:06:41 +0100

    [Oracle] Add IPFS verification

--- oracle/lib/ipfs.dart
+++ oracle/lib/ipfs.dart
@@ -1,12 +1,37 @@
-import 'dart:typed_data';
+import 'dart:async';
+import 'dart:io';
 
 import './verifier.dart';
 
 class IpfsVerifier implements IVerifier {
-  final proxyUrl = 'https://ipfs.io/ipfs/';
+  final httpClient = HttpClient();
+  final proxyUrl = 'https://gateway.ipfs.io/ipfs/';
 
   @override
-  Future<bool> verify(String domain) {
-    
+  Future<bool> verify(String ipfsHash) async {
+    // Do a HTTP HEAD request to the URL of the gateway
+    // If the response code is 200, return true
+    // Also use a timeout of 5 seconds
+
+    final url = Uri.parse(proxyUrl + ipfsHash);
+    try {
+      final response = await _doHttpHeadRequest(url).timeout(
+        const Duration(seconds: 5),
+        onTimeout: () => throw TimeoutException('Timeout for IPFS check'),
+      );
+      return response.statusCode == 200;
+    } on TimeoutException {
+      return false;
+    } on SocketException catch (err, stackTrace) {
+      print('=== SocketException: $err ===');
+      print(stackTrace);
+      print('');
+      return false;
+    }
+  }
+
+  Future<HttpClientResponse> _doHttpHeadRequest(Uri url) async {
+    final request = await httpClient.headUrl(url);
+    return request.close();
   }
 }
